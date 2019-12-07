c17fca0f8c509576c93a43cc1fbcac5665c11998
Alessandro <alessandro.piccione.75@gmail.com>
2019-12-07 13:12:10 +0000

    Move the pre-build command into a dedicated file

---
+++ IntegrationTests/pre-build.cmd
@@ -0,0 +1,2 @@
+﻿:: copy the secret  API key (not part of the repository) into the build folder
+IF EXIST "$(ProjectDir)api keys.secret.yaml" "$(TargetDir)" (copy "$(ProjectDir)api keys.secret.yaml" "$(TargetDir)")

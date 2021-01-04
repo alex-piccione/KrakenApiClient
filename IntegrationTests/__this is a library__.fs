79d1b235d9040f6499083883aff10df50659aded
Alex <alessandro.piccione.75@gmail.com>
2021-01-04 23:17:27 +0000

    Add stupid file in Integration test to avoisd FS0988 Warinig when run dotnet test

---
+++ IntegrationTests/__this is a library__.fs
@@ -0,0 +1,7 @@
+﻿module __this_is_a_library__
+
+// to avoid the FS0988 Warning when run dotnet build and dotnet test
+// https://stackoverflow.com/questions/15159387/how-to-resolve-main-module-of-program-is-empty-nothing-will-happen-when-it-is
+
+do()
+

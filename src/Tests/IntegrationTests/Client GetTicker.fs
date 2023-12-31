dd810dfcf50081b3997fd8276c47c6f6b47d5357
Alex <alessandro.piccione.75@gmail.com>
2023-12-31 13:08:04 +0000

    update packages

--- src/Tests/IntegrationTests/Client GetTicker.fs
+++ src/Tests/IntegrationTests/Client GetTicker.fs
@@ -2,7 +2,6 @@
 module IntegrationTests.Client.GetTicker
 
 open System
-open System.Collections.Generic
 open NUnit.Framework
 open FsUnit
 open Alex75.KrakenApiClient
@@ -20,5 +19,5 @@ let GetTicker (main:string, other:string) =
     ticker.Pair |> should equal (pair)
 
 [<Test>]
-let ``GetTicker when asset does not exists`` ()  =
+let ``GetTicker when asset does not exists`` () =
     (fun () -> client.GetTicker(CurrencyPair("usd", "eth")) |> ignore ) |> should throw typeof<Exception>

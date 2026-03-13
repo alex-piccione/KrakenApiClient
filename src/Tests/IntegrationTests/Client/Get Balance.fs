b180cf52ce1072bd9adc19114070bafa37b6e8d6
Alex <alessandro.piccione.75@gmail.com>
2026-03-13 21:34:39 +0100

    update packages

--- src/Tests/IntegrationTests/Client/Get Balance.fs
+++ src/Tests/IntegrationTests/Client/Get Balance.fs
@@ -14,5 +14,6 @@ let ``GetBalance()`` () =
     balance.HasCurrency(Currency.GBP) |> should be True
     balance.HasCurrency(Currency.BTC) |> should be True
     balance.HasCurrency(Currency.DOT) |> should be True
+    balance.HasCurrency(Currency("SOL")) |> should be True
     balance[Currency.DOT].Total |> should be (greaterThan 0m)
     //balance[Currency.DOT].Stacking |> should be (greaterThan 0m)

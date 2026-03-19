3ae9c59b22f4665cabeba5ab10d2c19c8dd0beec
Alex <alessandro.piccione.75@gmail.com>
2026-03-20 00:04:22 +0100

    feat: create form from data

--- src/Tests/UnitTests/Signer.test.fs
+++ src/Tests/UnitTests/Signer.test.fs
@@ -9,7 +9,6 @@ let ``createSignature produce expected signature as per official documentation e
     let privateKey = "kQH5HW/8p1uGOVjbgWA7FunAmGO8lsSUXNsu3eow76sz84Q18fWxnyRzBHCd3pd5nE9qa99HAZtuZuj6F1huXg=="
     let nonce = "1616492376594"
     let path = "/0/private/AddOrder"
-    let expectedPayload = "nonce=1616492376594&ordertype=limit&pair=XBTUSD&price=37500&type=buy&volume=1.25"
     let expectedSignature = "4/dpxb3iT4tp/ZCVEwSnEsLxx0bqyhLpdfOpc6fn7OR8+UClSV5n9E6aSS8MPtnRfp32bAb0nmbRn6H8ndwLUQ=="
 
     let data = Map.ofSeq<string, obj> [
@@ -21,6 +20,5 @@ let ``createSignature produce expected signature as per official documentation e
         "volume", "1.25"
     ]
 
-    let payload, signature = utils.createSignature path privateKey data nonce
-    payload |> should equal expectedPayload
+    let signature = utils.createSignature path privateKey data nonce
     signature |> should equal expectedSignature
